c87a45e980e34335f77dcab2241ce8264c1021b4
Luisgarchi <luisgarchi@live.com>
2023-08-07 16:20:13 +0200

    Package config of scheduler

--- NewsWebApplication/apptools.py
+++ NewsWebApplication/apptools.py
@@ -1,5 +1,6 @@
 from pygooglenews import GoogleNews
 from nlptools import get_publisher_from_url, url_google_to_original
+from newsplease import NewsPlease
 import newspaper
 import validators
 import datetime
@@ -41,21 +42,20 @@ def proccessGoogleNews(gn_news):
 
         try: 
             article = newspaper.Article(url=url, language='en')
+            article = NewsPlease.from_url(url=url, language='en')
             article.download()
             article.parse()
         except:
             continue
 
         # Convert to dictionary and drop unnessary attributes
-        tidy_article = filter_article_attributes(article)
+        tidy_article = filter_newspaper3k_attributes(article)
         articles.append(tidy_article)
     
     return articles
 
 
-
-def filter_article_attributes(article_obj):
-
+def filter_newspaper3k_attributes(article_obj):
 
     article = {
         "title": str(article_obj.title),
@@ -68,6 +68,15 @@ def filter_article_attributes(article_obj):
 
     return article
 
+def filter_newsplease_attributes(article_obj):
 
+    article = {
+        "title": str(article_obj.title),
+        "text": str(article_obj.text),
+        "published_date": str(article_obj.publish_date),
+        "summary": str(article_obj.summary),
+        "url" : str(article_obj.url),
+        "publisher": get_publisher_from_url(article_obj.url)
+    }
 
-
+    return article
