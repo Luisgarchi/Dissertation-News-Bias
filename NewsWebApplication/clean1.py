a4741b61993bc2c70eeaa0c10c7d6fa358f4d773
Luisgarchi <luisgarchi@live.com>
2023-08-31 17:16:54 +0200

    Finished coref and NED

--- NewsWebApplication/clean1.py
+++ NewsWebApplication/clean1.py
@@ -3,7 +3,16 @@ from spacy import displacy
 from newsplease import NewsPlease
 from pprint import pprint
 from collections import Counter
+import numpy as np
 import re
+
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 # Create pipeline
 nlp = spacy.load("en_core_web_trf")
@@ -39,54 +48,139 @@ class Entity():
         self.ent_obj = []
         self.head = None
         self.descriptors = []
+        self.chunk_descriptors = []
+        self.adjectives = []
+        self.related_entities = []
         self.coref_clusters = []
 
     def add_knowledge_base_info(self, kb_candidates_info):
         self.kb_candidates = kb_candidates_info
 
+    def resolve_kb_candidates(self):
+
+        # Case 1) There is only one candidate - assign the caniddate as the knowledge base ID - kb_id
+        if len(self.kb_candidates) == 1:
+            self.kb_id = list(self.kb_candidates.keys())[0]
+
+        elif len(self.kb_candidates)> 1:
+            
+            # if the entity is of type person filter candidate for wikidata instance of human (Q5)
+            if self.type == 'PERSON':
+
+                delete_keys = []
+                for can_key in self.kb_candidates.keys():
+                    if self.kb_candidates[can_key]['instance'] not in ['Q5', None]:
+                        delete_keys.append(can_key)
+                
+                for del_key in delete_keys:
+                    del self.kb_candidates[del_key] 
+
+            key_words = [self.head]
+            extend_list = [self.descriptors, self.related_entities, self.adjectives, self.chunk_descriptors]
+            for extend in extend_list:
+                key_words.extend(extend)
+
+            key_words = set([word.lower() for word in key_words])
+            
+            for can_key in self.kb_candidates.keys():
+                self.kb_candidates[can_key]['desc_doc'] = nlp(self.kb_candidates[can_key]['description'])
+                sentences = [x for x in self.kb_candidates[can_key]['desc_doc'].sents]
+                first_sentence = sentences[0].text
+
+                tokenized_words = tokenize_number_words(remove_punctuation(first_sentence), 50, sw)
+                self.kb_candidates[can_key]['tokenized'] = unique_maintain_order(tokenized_words)
+
+                score = 0
+                for token in self.kb_candidates[can_key]['tokenized']:
+                    if token in key_words:
+                        score += 1
+                
+                self.kb_candidates[can_key]['score'] = score
+
+            heighest_score = 0
+            best_key = ''
+            for can_key in self.kb_candidates.keys():
+                if self.kb_candidates[can_key]['score'] > heighest_score:
+                    heighest_score = self.kb_candidates[can_key]['score']
+                    best_key = can_key
+
+            self.kb_id = best_key
+
+
+
     def add_ent_obj(self, spacy_ent_obj):
         self.ent_obj.append(spacy_ent_obj)
     
     def add_descriptor(self, descriptor):
-        self.descriptors.append(descriptor)
+        if descriptor not in self.descriptors and descriptor not in self.head:
+            self.descriptors.append(descriptor)
 
     def set_head(self, head):
         self.head = head
-        
-        # Make a call on the set_descriptors
-        for ent in self.ent_obj:
-            for token in ent:
-                if token.head.text == self.head and token.dep_ == 'compound' and token.text not in self.descriptors:
-                    self.descriptors.append(token.text)
+
+    def add_chunk_descriptor(self, descriptor):
+        if (descriptor not in self.chunk_descriptors 
+            and descriptor not in self.adjectives 
+            and descriptor not in self.descriptors 
+            and descriptor not in self.head):
+            self.chunk_descriptors.append(descriptor)
     
+    def add_adjective(self, adjective):
+        if adjective not in self.adjectives:
+            self.adjectives.append(adjective)
 
-
-    def add_descriptors(self, descriptor):
-
-        # str -> None
-
-        # x = ['PROPN', 'NOUN', 'ADJ']
-
-        if descriptor not in self.descriptors:
-            self.descriptors.append(descriptor)
-
-    def add_chunk_descriptors(self, chunk):
-
-        # span -> None
-
-        for token in chunk:
-            if token.text != self.head:
-                self.add_descriptors(token.text)
+    def add_related_entity(self, entity_name):
+        if entity_name not in self.related_entities:
+            self.related_entities.append(entity_name)
 
     def add_coreference_clusters(self, cluster):
         self.coref_clusters.extend(cluster)
-
-
 
     def __repr__(self):
         return f"{self.name}, {self.type}, {self.count}"
         
 
+
+def unique_maintain_order(seq):
+    # https://stackoverflow.com/questions/480214/how-do-i-remove-duplicates-from-a-list-while-preserving-order
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
+
+
+def remove_punctuation(text):
+    
+    # Removes punctuation (str -> str)
+    
+    remove_punctuation = string.punctuation + "‘’’—“”"
+    return text.lower().translate(str.maketrans('', '', remove_punctuation))
+
+
+# We have to remove the punctation from stop words because 
+# punctuation will be remove from the text, 
+# inorder for them to match they must be processed the same (or removed before)
+
+sw = [remove_punctuation(word) for word in stopwords.words('english')]
+
+
+
+def tokenize_number_words(text, number, sw):
+    
+    # Tokenizes words, removes stop words and returns the first n number of tokens (str -> list)
+    
+    tokenized = word_tokenize(text)
+    
+    i = 0
+    while i < number and i < len(tokenized):
+        if tokenized[i] in sw: 
+            del tokenized[i]
+        else: 
+            i += 1
+    
+    if number < len(tokenized):
+        del tokenized[number:]
+    
+    return tokenized
 
 
 
@@ -123,7 +217,7 @@ class DocResolve:
             j = 0
             while entities_most_common and j < len(entities_most_common):
                 ent_B = entities_most_common[j]
-                if ent.type == ent_B.type and (ent.name in ent_B.name or ent_B.name in ent.name):
+                if ent.type == ent_B.type and (ent.name.lower() in ent_B.name.lower() or ent_B.name.lower() in ent.name.lower()):
                     candidates.append(ent_B)
                     entities_most_common.pop(j)
                     continue
@@ -169,16 +263,42 @@ class DocResolve:
     
 
     def kb_preprocess(self, entity):
+        
+        """HELPER FUNCTION"""
 
-        count = Counter([(ent._.kb_qid, ent._.description) for ent in doc.ents if (ent.text in entity.name) or (entity.name in ent.text)]).most_common()
-        unpack_count = [(candidate[0][0], candidate[0][1], candidate[1]) for candidate in count]
-        entity.add_knowledge_base_info(unpack_count)
+        def get_instance(properties):
+
+            if not properties:
+                return None
+
+            else:
+                for property in properties:
+                    if property['propertyName'] == 'instance of':
+                        return property['value']
+
+        kb_candidates = {}
+
+        for ent in doc.ents:
+            if (ent.text.lower() in entity.name.lower()) or (entity.name.lower() in ent.text.lower()):
+                if ent._.kb_qid:      # Make sure not None
+                    if ent._.kb_qid not in kb_candidates:
+                        kb_candidates[ent._.kb_qid] = {}
+                        kb_candidates[ent._.kb_qid]['count'] = 1
+                        kb_candidates[ent._.kb_qid]['description'] = ent._.description
+                        kb_candidates[ent._.kb_qid]['instance'] = get_instance(ent._.other_ids)
+                    else:
+                        kb_candidates[ent._.kb_qid]['count'] += 1
+
+        
+        #count = Counter([(ent._.kb_qid, ent._.description) for ent in doc.ents if (ent.text.lower() in entity.name.lower()) or (entity.name.lower() in ent.text.lower())]).most_common()
+        #unpack_count = [(candidate[0][0], candidate[0][1], candidate[1]) for candidate in count]
+        entity.add_knowledge_base_info(kb_candidates)
     
 
     def get_ent_objs(self, entity):
 
         for ent in self.doc.ents:
-            if (entity.name in ent.text) or (ent.text in entity.name):
+            if (entity.name.lower() in ent.text.lower()) or (ent.text.lower() in entity.name.lower()):
                 entity.add_ent_obj(ent)
 
 
@@ -199,17 +319,16 @@ class DocResolve:
                     candidate_head[word] = 0
 
                 for token in self.doc[ent.start : ent.end]:
-                    print("EXECUTED")
                     for key in candidate_head.keys():
                         if token.head.text == key:
                             candidate_head[key] += 1
 
                 head = max(candidate_head, key = candidate_head.get)
-                entity.set_head(head)
+                entity.set_head(head.capitalize())
 
 
 
-    def get_descriptors(self, entity):
+    def get_ner_descriptors(self, entity):
         """Maybe can take in an argument as well with potential descriptors of the previous get_heads"""
         """Can also use the coreferences resolved that are not prononuns e.g. modofies"""
         """Use chunks as well"""
@@ -217,22 +336,37 @@ class DocResolve:
         for ent in entity.ent_obj:
 
             for token in self.doc[ent.start : ent.end]:
-                if token.head.text == entity.head and token.dep_ == 'compound':
-                    entity.add_descriptors(token.text)
+                if token.head.text.lower() == entity.head.lower() and token.dep_ == 'compound':
+                    entity.add_descriptor(token.text.capitalize())
 
 
         # Check noun phrases that can describe the ent
         for chunk in self.doc.noun_chunks:
-            if entity.head in chunk.root.text:
-                entity.add_chunk_descriptors(chunk)
+        
+            if entity.name.lower() in chunk.text.lower():
+                if entity.head in chunk.root.text:
+                    for token in chunk:
+                        if token.pos_ in ['PROPN', 'NOUN', 'ADJ']:
+                            entity.add_chunk_descriptor(token.text)
+                
+
+                descriptors = [token.text for token in chunk if token.pos_ in ['PROPN', 'NOUN'] and entity.head.lower() in token.head.text.lower()]
+                for descriptor in descriptors:
+                    entity.add_descriptor(descriptor)
+
+                adjectives = [token.text for token in chunk if token.pos_ in ['ADJ'] and entity.head.lower() in token.head.text.lower()]
+                for adjective in adjectives:
+                    entity.add_adjective(adjective)
+                    
 
 
 
 
     def get_coref_clusters(self):
-        """Helper function"""
-        def heads_in_cluster(entities, cluster):
 
+        """Helper function"""
+
+        def heads_in_cluster(entities, cluster):
             # (list, list) -> True
             heads = [entity.head for entity in entities]
             for coref in cluster:
@@ -240,9 +374,10 @@ class DocResolve:
                     return True
             return False
         
-
+        # Create a mapping from entity name to an entity object
         entities_obj_map = {entity.name: entity for entity in self.entities}
         
+        # Filter the relevant coreference spans. Criteria: 1) Only head clusters. 2) Contains a found entity head 
         coref_clusters = {
             key: coref for key, coref in self.doc.spans.items() 
             if re.match(r"coref_head_clusters_*", key)
@@ -250,9 +385,16 @@ class DocResolve:
             }
 
         for coref_span in coref_clusters.items():
-            key, coreferences = coref_span[0], coref_span[1]
+
+            # Iterate over keys a and values for the filtered clusters 
+            key = coref_span[0]
+            coreferences = coref_span[1]
+
+            # Get the cluster key number
             key = key.split('_')[-1]
 
+            # Find the respective entity for the coreference groups by using a voting system 
+            # if the head of an entity appears in the coreference cluster head
             coref_candidates = {}
             for entity in self.entities:
                 for reference_head in coreferences:
@@ -263,20 +405,21 @@ class DocResolve:
                             coref_candidates[entity.name] += 1
                     #Pronouns are ignored since we are only interested in finding mentions of the found entities
                 
+            cluster_key = 'coref_clusters_' + key
 
             if len(coref_candidates) == 1:
                 # There is only one associated coreference cluster
                 associated_entity = list(coref_candidates.keys())[0]
-                cluster_key = 'coref_clusters_' + key
-                entities_obj_map[associated_entity].add_coreference_clusters(self.doc.spans[cluster_key])
+                head_key = 'coref_head_clusters_' + key
+
+                self.process_coreference_span(entities_obj_map[associated_entity], self.doc.spans[cluster_key], self.doc.spans[head_key])
             else: 
-                # If there is more than one entity in the span we need to find the most common head 
-                # Pronouns are assign to the most common entity
+                # If there is more than one entity in the coref head cluster find the most common head 
+                most_common_entity = max(coref_candidates, key = coref_candidates.get)
+
+                # The pronouns of the cluster are assign to the most common entity
                 # Other head mentions are added to their respective entiity
 
-                most_common_entity = max(coref_candidates, key = coref_candidates.get)
-                cluster_key = 'coref_clusters_' + key
-                
                 other_ents = list(coref_candidates.keys())
                 other_ents.remove(most_common_entity)
 
@@ -285,15 +428,63 @@ class DocResolve:
                     for entity_name in other_ents:
                         
                         #Get the coreference span in question
-                        reference_span = [self.doc.spans[cluster_key][i]]
+                        reference_span = self.doc.spans[cluster_key][i]
                         """Potential Bug"""
                         # If it is the head or a descriptor (typically compound dependecy) of another entity add the coreference span to that entity
                         if reference_head.text == entities_obj_map[entity_name].head or reference_head.text in entities_obj_map[entity_name].descriptors:
-                            entities_obj_map[entity_name].add_coreference_clusters(reference_span)
+                            self.process_coreference(entities_obj_map[entity_name], reference_span, reference_head)
                         else: 
                             # Otherwise Save it to the most common
-                            entities_obj_map[most_common_entity].add_coreference_clusters(reference_span)
+                            self.process_coreference(entities_obj_map[most_common_entity], reference_span, reference_head)
+    
+
+    def process_coreference_span(self, entity, coref_span, coref_head):
+
+        for i in range(len(coref_span)):
+            self.process_coreference(entity, coref_span[i], coref_head[i])
+
+    def process_coreference(self, entity, reference, head):
         
+
+        if head[0].pos_ in ['PROPN','NOUN']:            
+            entity.add_descriptor(head.text.capitalize())
+
+        # Check for other entities inside
+        other_entities = [ent for ent in self.entities if ent.name != entity.name]
+
+        for other_ent in other_entities:
+
+            for ent_obj in other_ent.ent_obj:
+                if ent_obj.start >= reference.start and ent_obj.end <= reference.end:
+                    entity.add_related_entity(other_ent.name)
+
+
+        # Get descriptors
+
+        # Add the coreference
+
+
+        
+
+
+
+
+    def get_coreference_descriptors(self, entity, reference_span, reference_head):
+
+        # Check if there is a way to find overlapping span groups.
+        """
+        other_entities = [ent for ent in self.entities if ent.name != entity.name]
+        menthion of other ents 
+        """
+        
+        # Normal Descriptors
+        descriptors = [token.text for token in reference_span
+                       if token.pos_ in ['PROPN', 'NOUN', 'ADJ'] 
+                       and token.head.text == reference_head.text
+                       and token.text not in entity.head]
+
+        for descriptor in descriptors:
+            entity.add_descriptor(descriptor.capitalize())
 
 
     def NED_preprocess(self):
@@ -303,10 +494,12 @@ class DocResolve:
             self.kb_preprocess(entity)
             self.get_ent_objs(entity)
             self.get_heads(entity)
-            # self.get_descriptors(entity) # Need to prevent adding pronouns might get in the way of coreference resolution
-        
+            self.get_ner_descriptors(entity) # Need to prevent adding pronouns might get in the way of coreference resolution
         
         coreference_clusters = self.get_coref_clusters()
+
+        for entity in self.entities:
+            entity.resolve_kb_candidates()
     
 
 
@@ -324,7 +517,7 @@ Split get descriptors into two methods one for, compound dependencies of the hea
 
 Create associated entity method on doc resolve
 
-filter the descriptors to add only PROPN, NOUN (if PROPN then check for asociated entities) and ADJ if token head is the the orgingial head of coref
+filter the descriptors to add only PROPN, NOUN (if PROPN then check for asociated entities) and ADJ if token head is the the original head of coref
 
 1. Check for new potential heads to add to descriptors,
 2.
@@ -341,36 +534,37 @@ filter the descriptors to add only PROPN, NOUN (if PROPN then check for asociate
 
 
 
-mydoc = DocResolve(doc, top_x = 5)
-
-"""
-for ents in mydoc.entities:
-    for ent in ents.ent_obj:
-        print(ent.text, ent.start, ent.end, [(token.text, token.pos_, token.dep_, token.head) for token in doc[ent.start : ent.end]])
-"""
+mydoc = DocResolve(doc, top_x = 10)
 
 
 print()
 for ent in mydoc.entities:
-    print(ent.head, ": ",ent.descriptors)
+    print(ent.head, ":\n descriptors: ", ent.descriptors,
+          "\n related ents: ", ent.related_entities,
+          "\n chunk descriptor: ", ent.chunk_descriptors,
+          "\n adjectives: ", ent.adjectives)
+    print()
 
 
 
 
-"""
+
+
+# 'nsubj', 'dobj', 'pobj'
+
+
 print()
 for ent in mydoc.entities:
-    print(ent.head, ": \n")
-    for item in ent.coref_clusters:
-        print([(token.text, token.pos_, token.dep_, token.head.text) for token in item])
+    print(ent.head, ": ", ent.kb_id)
+    pprint(ent.kb_candidates)
+    print()
 
 
-
-pprint(doc.spans)
 
 """
-
-
+for item in ent.coref_clusters:
+    print([(token.text, token.pos_, token.dep_, token.head.text) for token in item])
+"""
 
 
 
